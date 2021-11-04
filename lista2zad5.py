2c802d2021d07cb447a6eb7f06253d164b04cbb3
Jakub Siedlecki <siedlecki.jakub14.02@gmail.com>
2021-11-04 10:18:59 +0100

    zadanie5 lista2

--- lista2zad5.py
+++ lista2zad5.py
@@ -1,5 +1,5 @@
 napis=input("Podaj napis o parzystej liczbie znaków: ")                                                                                                           
-połowa=len(napis)//2                                                                 # 'len()' zwraca ilość znaków w napisie, "//2" potrzebuję znać połowę długości wyrazu bez reszty
-część1=napis[0:połowa]+"[Python]"                                                    # pierwsza połowa nowego wyrazu 
-część2=napis[połowa:]                                                                # druga połowa nowego wyrazu     
-print("Napis powstały po wstaieniu do środala '[Python]': ",część1+część2)             # nowy wyraz  
+połowa=len(napis)//2                                                                                      # 'len()' zwraca ilość znaków w napisie, "//2" potrzebuję znać połowę długości wyrazu bez reszty
+część1=napis[0:połowa]+"[Python]"                                                                         # pierwsza połowa nowego wyrazu 
+część2=napis[połowa:]                                                                                     # druga połowa nowego wyrazu     
+print("Napis powstały po wstawieniu do środka pierwotnego napisu '[Python]': ",część1+część2)             # nowy wyraz  
